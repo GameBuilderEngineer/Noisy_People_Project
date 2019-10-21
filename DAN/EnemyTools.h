4ca8463404702b430e759397d449e9aa34456570
kong9812 <48704407+kong9812@users.noreply.github.com>
2019-10-21 13:08:19 +0900

    【マージ】マージの準備@サイ

--- DAN/EnemyTools.h
+++ DAN/EnemyTools.h
@@ -17,7 +17,7 @@
 //===================================================================================================================================
 //【マクロ】
 //===================================================================================================================================
-#define CHUNK_ID				(4)
+#define ENEMY_CHUNK_ID		(4)
 #define ENMY_CHUNK			("ENMY")
 #define EFMT_CHUNK			("EFMT")
 #define ENEMY_FILE_PATH		("enemy.enemy")
@@ -27,17 +27,17 @@
 //===================================================================================================================================
 typedef struct	//ENMYチャンク
 {
-	char chunkId[CHUNK_ID];	//チャンクID
+	char chunkId[ENEMY_CHUNK_ID];	//チャンクID
 	short size;				//以後のサイズ(ファイルのサイズ - 8byte)
 	short enemyMax;			//敵の数
 }ENEMY_ENMY;
 
 typedef struct	//EFMTチャンク
 {
-	char chunkId[CHUNK_ID];	//チャンクID
+	char chunkId[ENEMY_CHUNK_ID];	//チャンクID
 	short size;				//このチャンクのサイズ
 	short enemyId;			//敵のID
-	short enemyType;			//敵の種類
+	short enemyType;		//敵の種類
 	short enemyState;		//敵の状態
 	float posX;				//敵の生成位置(X)
 	float posY;				//敵の生成位置(Y)
@@ -67,6 +67,9 @@ public:
 	enemyNS::ENEMYSET GetEnemySet(short enemyId);
 	int GetEnemyMax(void);
 	void SetEnemy(short enemyId, short enemyType, short enemyState, const D3DXVECTOR3 pos, const D3DXVECTOR3 dir);
+
+#ifdef _DEBUG
+	//関数
 	void outputEnemyToolsGUI(const D3DXVECTOR3 pos, const D3DXVECTOR3 dir);
 
 	//描画用
@@ -74,6 +77,7 @@ public:
 	void update();
 	void render(D3DXMATRIX view, D3DXMATRIX projection, D3DXVECTOR3 cameraPositon);
 	void generate(D3DXVECTOR3 position);
+#endif
 
 private:
 
@@ -83,11 +87,13 @@ private:
 	int EnemyListboxType;					//エネミーの種類(リストボックス)
 	int EnemyListboxState;					//エネミーの状態(リストボックス)
 
+#ifdef _DEBUG
 	//描画用
 	StaticMeshObject* renderer;
 	bool needUpdate;
+#endif
 
-	//関数
+//関数
 	void OutputEnemyFile(void);				//エネミーファイルの書き出し処理
 	void CreatNewEnemyFile(void);			//エネミーファイルの新規作成
 	void AddEnemyFormat(short enemyType, short enemyState, const D3DXVECTOR3 pos, const D3DXVECTOR3 dir);
