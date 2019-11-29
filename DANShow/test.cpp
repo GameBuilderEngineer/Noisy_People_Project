22109296b53880ded138c36a9fb9d916e272404e
kong9812 <48704407+kong9812@users.noreply.github.com>
2019-11-29 10:01:15 +0900

    【DShow】微調整@サイ

--- DANShow/test.cpp
+++ DANShow/test.cpp
@@ -6,9 +6,6 @@ TestPic::TestPic(HWND hWnd)
 	LoadTexture(GetDevice(), &testpic, "wood.jpg");
 
 	testpic.pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	RECT rect = { 0 };
-	GetClientRect(hWnd, &rect);
-
 	testpic.size = D3DXVECTOR2(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	MakeVtx4PointData(&testpic);
