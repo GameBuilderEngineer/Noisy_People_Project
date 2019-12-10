6807c9208f37750976506660784609c3540fa316
Local kanno tatsuki <gp11ckanno08@gmail.com>
2019-12-10 21:34:50 +0900

    ビルボード修正

--- DAN/InstancingBillboard.cpp
+++ DAN/InstancingBillboard.cpp
@@ -224,9 +224,12 @@ void InstancingBillboard::render(D3DXMATRIX view, D3DXMATRIX projection, D3DXVEC
 	effect->Begin(0, 0);
 
 	int passValue = 0;
-	if (renderType & Y_BILLBOARD_PASS)	passValue += 3;
-	if (renderType & OFF_BILLBOARD_PASS)passValue += 3;
-	switch (renderType & ~(Y_BILLBOARD_PASS&OFF_BILLBOARD_PASS))
+	if (renderType & OFF_BILLBOARD_PASS)
+	{passValue += 6;}
+	else if (renderType & Y_BILLBOARD_PASS) 
+	{ passValue += 3; }
+
+	switch (renderType & ~(Y_BILLBOARD_PASS|OFF_BILLBOARD_PASS))
 	{
 	case NORMAL_PASS:		effect->BeginPass(0+passValue);break;
 	case TRANSPARENT_PASS:	effect->BeginPass(1+passValue);break;
