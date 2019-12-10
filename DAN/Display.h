a6b8480e75f831c0a32b218764346594cfc9f247
Local kanno tatsuki <gp11ckanno08@gmail.com>
2019-12-10 20:13:48 +0900

    ディスプレイシーン実装中

--- DAN/Display.h
+++ DAN/Display.h
@@ -1,11 +1,61 @@
+//===================================================================================================================================
+//【Display .h】
+// [作成者]HAL東京GP12A332 11 菅野 樹
+// [作成日]2019/12/10
+// [更新日]2019/12/10
+//===================================================================================================================================
 #pragma once
+
+//===================================================================================================================================
+//【インクルード】
+//===================================================================================================================================
 #include "AbstractScene.h"
+#include "StaticMeshRenderer.h"
+#include "TreeManager.h"
+
+//===================================================================================================================================
+//【名前空間】
+//===================================================================================================================================
+namespace DisplayNS
+{
+	const D3DXQUATERNION	CAMERA_RELATIVE_QUATERNION	= D3DXQUATERNION(0.0f,300.0f, -300.0f, 0.0f);
+	const float				CAMERA_SPEED				= 30.0f;
+
+
+	const int				SERVER_MODE					= 0x00000001;
+	const int				CLIENT_MODE					= 0x00000002;
+}
+
+//===================================================================================================================================
+//【ディスプレイシーンクラス】
+//===================================================================================================================================
 class Display : public AbstractScene
 {
+private:
+	//フィールド
+	Object*							testField;			//フィールド
+	StaticMeshRenderer*				testFieldRenderer;	//フィールドレンダラー
+
+	TreeManager*					treeManager;		//ツリー
+
+	float							syncTimer;			//同期タイマー
+
 public:
 	Display();
 	~Display();
 
+	virtual void initialize() override;
+	virtual void uninitialize(void) override;
+	virtual void update(float _frameTime) override;
+	virtual void render() override;
+	virtual void collisions(void) override;
+	virtual void AI(void) override;
 
+	void render3D(Camera currentCamera);
+	//void renderUI();
+
+#ifdef _DEBUG
+	virtual void createGUI() override;
+#endif
 };
 
