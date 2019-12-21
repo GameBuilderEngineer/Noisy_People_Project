a5884f3db526143b942772649e26ba227f1e552a
中込和輝 <astora.home@gmail.com>
2019-12-21 22:21:32 +0900

    サウンド修正

--- DAN/GameMaster.h
+++ DAN/GameMaster.h
@@ -53,7 +53,7 @@ namespace gameMasterNS {
 	};
 
 	const float OPENING_TIME			= 10.0f;				//10•b
-	const float GAME_TIME				= 60.0f * 4.0f;			//4•ª
+	const float GAME_TIME = 70.0f;// 60.0f * 4.0f;			//4•ª
 	const float COUNT_DOWN_TIME			= 3.0f;					//3•b
 	const float ENDING_TIME				= 5.0f;					//5•b
 
